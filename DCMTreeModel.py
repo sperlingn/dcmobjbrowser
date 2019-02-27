6913eae24405a47204352b1bc9e2dc5f76ad1f15
Nicholas Sperling <nicholas.sperling@utoledo.edu>
2019-02-27 17:23:44 -0500

    Cleanup to meet PEP, simplify settings.
    Signed-off-by: Nicholas Sperling <nicholas.sperling@utoledo.edu>

--- DCMTreeModel.py
+++ DCMTreeModel.py
@@ -5,79 +5,73 @@ Created on Mon Feb 25 14:25:36 2019
 @author: nsperling
 """
 
-import objbrowser.treemodel
+from objbrowser.treemodel import TreeModel
 from objbrowser.treeitem import TreeItem
-#import logger
-#from collections import OrderedDict
 from pydicom.dataset import Dataset, DataElement
-#from pydicom.tag import tag_in_exception
 
 
-class DCMTreeModel(objbrowser.treemodel.TreeModel):
+class DCMTreeItem(TreeItem):
+    """
+    Extension of objbrowser.treeitem.TreeItem to allow for initialization with
+    no children.  New init function only.
+    """
+
+    def __init__(self, obj, name, obj_path, is_attribute,
+                 parent=None, has_children=True):
+
+        super().__init__(obj, name, obj_path, is_attribute, parent)
+
+        self.has_children = has_children
+
+
+class DCMTreeModel(TreeModel):
     # Class for pydicom objects to sensibly fetch object children for tree.
     # Should override _fetchObjectChildren to allow reasonable data in tree.
     _TREAT_LIKE_OBJECTS = True
-    
+
     def _fetchObjectChildren(self, obj, obj_path):
-        """ Fetches the children of a Python object. 
+        """ Fetches the children of a Python object.
             Returns: list of TreeItems
         """
         if not isinstance(obj, (Dataset, DataElement)):
             # Not a dicom data element/dataset.  Use normal process
             return super()._fetchObjectChildren(obj, obj_path)
-        
+
         # Start with dataset
         try:
             if obj.VR == 'SQ':
                 # Sequence so children are a list and there are no attributes.
-                tree_items = list([TreeItem(de, i, '{}[{}]'.format(obj_path, i),
-                                   False) for i,de in enumerate(obj)])
-                return tree_items
-            
-                # Old tree method.
-                obj_children, path_strings, is_attr_list = map(list, 
-                                   zip(*[((i,de), '{}[{}]'.format(obj_path, i),
-                                          False) for i,de in enumerate(obj)]))
+                return [DCMTreeItem(de, i, '{}[{}]'.format(obj_path, i), False)
+                        for i, de in enumerate(obj)]
         except AttributeError as e:
             # Ignore attribute error for datasets
-            if not isinstance(obj, Dataset): 
+            if not isinstance(obj, Dataset):
                 raise e
 
-        # All descendents of Dataset and DataElement should be DataElements
-        obj_children = []
-        path_strings = []
-        
-        # Method for non sequences. (Treat like attribute?)
-        if self._TREAT_LIKE_OBJECTS: # Treat like objects
-            obj_children = list([(de.tag, de) for de in obj])
-            path_strings = list(['{}.{}'.format(obj_path, de.tag) for de in 
-                                 obj])
-            is_attr_list = [False] * len(obj_children)
-        
-        if not self._TREAT_LIKE_OBJECTS: # Treat like attributes
-            # handle non-sequence children as attributes
-            obj_children = list([(de.tag, de.value) for de in obj  
-                                 if de.VR != 'SQ'])
-            path_strings = list(['{}.{}'.format(obj_path, de.tag) for de in 
-                                 obj if de.VR != 'SQ'])
-            is_attr_list = [True] * len(obj_children)
-            
-            # handle sequence children as objects
-            obj_children_sq = list([(de.tag, de) for de in obj 
-                                    if de.VR == 'SQ'])
-            path_strings_sq = list(['{}.{}'.format(obj_path, de.tag) for de in
-                                    obj if de.VR == 'SQ'])
-            is_attr_list_sq = [False] * len(obj_children_sq)
-            
-            obj_children.extend(obj_children_sq)
-            path_strings.extend(path_strings_sq)
-            is_attr_list.extend(is_attr_list_sq)
-            
+        return [DCMTreeItem(de,
+                            '{} --- {}'.format(de.tag, de.keyword),
+                            '{}.{}'.format(obj_path, de.tag),
+                            False,
+                            has_children=de.VR == 'SQ')
+                for i, de in enumerate(obj)]
+
+        """
+        Replaced with much cleaner version
+
+        obj_children = [(de.tag, de) for de in obj]
+        path_strings = ['{}.{}'.format(obj_path, de.tag) for de in obj]
+        is_attr_list = [de.VR != 'SQ' for de in obj]
+
         assert len(obj_children) == len(path_strings) == len(is_attr_list), \
-                "sanity check"
+            "sanity check"
         tree_items = []
-        for item, path_str, is_attr in zip(obj_children, path_strings, is_attr_list):
+        for item, path_str, is_attr in zip(obj_children,
+                                           path_strings,
+                                           is_attr_list):
             name, child_obj = item
-            tree_items.append(TreeItem(child_obj, name, path_str, is_attr))
-            
-        return tree_items    
+            child_item = TreeItem(child_obj, name, path_str, is_attr)
+            child_item.has_children = child_obj.VR == 'SQ'
+            tree_items.append(child_item)
+
+        return tree_items
+        """
--- browser.py
+++ browser.py
@@ -10,13 +10,14 @@ TODO: Replace treemodel with custom treemodel made for pydicom datasets.
 
 import objbrowser
 import pydicom
-#import sys
 from objbrowser.qtpy import QtWidgets
 from objbrowser.version import DEBUGGING, PROGRAM_NAME
-from objbrowser.attribute_model import (AttributeModel, safe_data_fn, 
-                                        DEFAULT_ATTR_DETAILS, SMALL_COL_WIDTH,
-                                        ATTR_MODEL_NAME, ATTR_MODEL_PATH)
+from objbrowser.attribute_model import (AttributeModel, safe_data_fn,
+                                        SMALL_COL_WIDTH, MEDIUM_COL_WIDTH,
+                                        ATTR_MODEL_NAME, ATTR_MODEL_PATH,
+                                        ATTR_MODEL_REPR)
 from DCMTreeModel import DCMTreeModel
+
 
 def safe_element_value(de):
     if de.VR == 'SQ':
@@ -24,23 +25,25 @@ def safe_element_value(de):
     else:
         return str(de.value)
 
-ATTR_TAG_KEYWORD = AttributeModel('Tag Name',
+VMVR_COL_WIDTH = 20
+
+ATTR_DCM_KEYWORD = AttributeModel('Tag Name',
     doc         = "The dicom tag keyword if known.", 
     data_fn     = safe_data_fn(lambda obj: obj.keyword),
     col_visible = True,  
-    width       = SMALL_COL_WIDTH)
+    width       = MEDIUM_COL_WIDTH)
 
 ATTR_DCM_VM = AttributeModel('VM',
     doc         = "DICOM Value Multiplicity.", 
     data_fn     = safe_data_fn(lambda obj: obj.VM),
     col_visible = True,  
-    width       = SMALL_COL_WIDTH)
+    width       = VMVR_COL_WIDTH)
 
 ATTR_DCM_VR = AttributeModel('VR',
     doc         = "DICOM Value Representation. (str, int, etc.)", 
     data_fn     = safe_data_fn(lambda obj: obj.VR),
     col_visible = True,  
-    width       = SMALL_COL_WIDTH)
+    width       = VMVR_COL_WIDTH)
 
 ATTR_DCM_VALUE = AttributeModel('Value',
     doc         = "Element Value", 
@@ -50,51 +53,63 @@ ATTR_DCM_VALUE = AttributeModel('Value',
 
 DEFAULT_DCM_ATTR_COLS = (
     ATTR_MODEL_NAME,
-    ATTR_MODEL_PATH,
-    ATTR_TAG_KEYWORD, 
     ATTR_DCM_VM,
     ATTR_DCM_VR,
+    ATTR_DCM_KEYWORD,
     ATTR_DCM_VALUE
 )
 
+DEFAULT_ATTR_DETAILS = (
+        ATTR_MODEL_PATH,
+        ATTR_DCM_KEYWORD,
+        ATTR_DCM_VALUE,
+        ATTR_MODEL_REPR
+)
+
+
 class DCMObj_Browser(objbrowser.ObjectBrowser):
     def __init__(self, obj=[],
-                 name = '',
-                 attribute_columns = DEFAULT_DCM_ATTR_COLS,
-                 attribute_details = DEFAULT_ATTR_DETAILS,
-                 show_callable_attributes = False,
-                 show_special_attributes = False,
-                 auto_refresh = False,
-                 refresh_rate = None,  # None uses value from QSettings
-                 reset = False):
+                 name='',
+                 attribute_columns=DEFAULT_DCM_ATTR_COLS,
+                 attribute_details=DEFAULT_ATTR_DETAILS,
+                 show_callable_attributes=False,
+                 show_special_attributes=False,
+                 auto_refresh=False,
+                 refresh_rate=None,  # None uses value from QSettings
+                 reset=False):
         """ Constructor
-        
+
             :param obj: any Python object or variable
             :param name: name of the object as it will appear in the root node
-            :param attribute_columns: list of AttributeColumn objects that define which columns
-                are present in the table and their defaults
-            :param attribute_details: list of AttributeDetails objects that define which attributes
-                can be selected in the details pane.
-            :param show_callable_attributes: if True rows where the 'is attribute' and 'is callable'
-                columns are both True, are displayed. Otherwise they are hidden. 
-            :param show_special_attributes: if True rows where the 'is attribute' is True and
-                the object name starts and ends with two underscores, are displayed. Otherwise 
-                they are hidden.
-            :param auto_refresh: If True, the contents refershes itsef every <refresh_rate> seconds.
-            :param refresh_rate: number of seconds between automatic refreshes. Default = 2 .
-            :param reset: If true the persistent settings, such as column widths, are reset. 
+            :param attribute_columns: list of AttributeColumn objects
+                that define which columns are present in the table
+                and their defaults
+            :param attribute_details: list of AttributeDetails objects that
+                define which attributes can be selected in the details pane.
+            :param show_callable_attributes: if True rows where the
+                'is attribute' and 'is callable' columns are both True,
+                are displayed. Otherwise they are hidden.
+            :param show_special_attributes: if True rows where the
+                'is attribute' is True and the object name starts and ends with
+                two underscores, are displayed. Otherwise they are hidden.
+            :param auto_refresh: If True, the contents refershes itsef every
+                <refresh_rate> seconds.
+            :param refresh_rate: number of seconds between automatic refreshes.
+                Default = 2.
+            :param reset: If true the persistent settings, such as column
+                widths, are reset.
         """
-        
 
-        
-        super(DCMObj_Browser, self).__init__(obj, name, attribute_columns, 
-                 attribute_details, show_callable_attributes, 
-                 show_special_attributes, auto_refresh, refresh_rate, reset)
-        
-        self._tree_model = DCMTreeModel(obj, name, attr_cols = self._attr_cols)
-        
+        super(DCMObj_Browser, self).__init__(obj, name, attribute_columns,
+                                             attribute_details,
+                                             show_callable_attributes,
+                                             show_special_attributes,
+                                             auto_refresh, refresh_rate, reset)
+
+        self._tree_model = DCMTreeModel(obj, name, attr_cols=self._attr_cols)
+
         self._proxy_tree_model.setSourceModel(self._tree_model)
-    
+
     def _setup_menu(self):
         """ Sets up the main menu.
         """
@@ -106,31 +121,36 @@ class DCMObj_Browser(objbrowser.ObjectBrowser):
         if DEBUGGING is True:
             file_menu.addSeparator()
             file_menu.addAction("&Test", self.my_test, "Ctrl+T")
-        
+
         view_menu = self.menuBar().addMenu("&View")
         view_menu.addAction("&Refresh", self.refresh, "Ctrl+R")
         view_menu.addAction(self.toggle_auto_refresh_action)
-        
+
         view_menu.addSeparator()
         self.show_cols_submenu = view_menu.addMenu("Table columns")
         view_menu.addSeparator()
         view_menu.addAction(self.toggle_callable_action)
         view_menu.addAction(self.toggle_special_attribute_action)
-        
+
         self.menuBar().addSeparator()
         help_menu = self.menuBar().addMenu("&Help")
         help_menu.addAction('&About', self.about)
-    
-    
+
+    def _setup_views(self):
+        """ Override creation of setup views to enable StretchLastSection
+        """
+        super()._setup_views()
+        self.obj_tree.header().setStretchLastSection(True)
+
     def _setup_menu_open_notused(self):
-        
+
         super(DCMObj_Browser, self)._setup_menu()
-        
+
         openaction = QtWidgets.QAction(self, text="&Open", shortcut="Ctrl+O")
         openaction.triggered.connect(self.getDCMtree_dialog)
-    
-        filemenu = next((x for x in 
-                         self.menuBar().findChildren(QtWidgets.QMenu) 
+
+        filemenu = next((x for x in
+                         self.menuBar().findChildren(QtWidgets.QMenu)
                          if x.title() == "&File"))
         fm_first = filemenu.findChildren(QtWidgets.QAction)[0]
         newsep = filemenu.insertSeparator(fm_first)
@@ -138,20 +158,19 @@ class DCMObj_Browser(objbrowser.ObjectBrowser):
 
     def getDCMtree_dialog(self):
         dcmdata = None
-        filename = QtWidgets.QFileDialog.getOpenFileName(self, 
-                                   "Select DICOM file to read...", "", 
-                                   "DICOM files (*.dcm);;All files (*)")[0]
+        title = "Select DICOM file to read..."
+        filefilter = "DICOM files (*.dcm);;All files (*)"
+        filename = QtWidgets.QFileDialog.getOpenFileName(self, title, "",
+                                                         filefilter)[0]
         try:
             dcmdata = pydicom.dcmread(filename)
         except pydicom.errors.InvalidDicomError:
             pass
-        #self._tree_model = objbrowser.treemodel.TreeModel(dcmdata, filename)
+        # self._tree_model = objbrowser.treemodel.TreeModel(dcmdata, filename)
         self._tree_model.populateTree(dcmdata, "<root>")
         self.refresh()
         self.setWindowTitle("{} - {}".format(PROGRAM_NAME, filename))
-        
 
 
-if __name__ ==  '__main__':
+if __name__ == '__main__':
     DCMObj_Browser.browse()
-    
